d62bb20f8c675962ec97d98fb9cb4751671004f7
Sorin Gore <soringoredev@gmail.com>
2025-06-11 19:41:20 +0300

    chore: Add mew RestController

---
+++ src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
@@ -0,0 +1,26 @@
+package com.example.rest_crud_app09.rest;
+
+import com.example.rest_crud_app09.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Munteanu", "Eugen"));
+        theStudents.add(new Student("Ojog", "Maria"));
+        theStudents.add(new Student("Gonzales", "Pedro"));
+
+        return theStudents;
+    }
+}
