708133af1eb392bd54e2995453ade86d95c6263e
Sorin Gore <soringoredev@gmail.com>
2025-06-11 19:37:44 +0300

    chore: Add new entity

---
+++ src/main/java/com/example/rest_crud_app09/entity/Student.java
@@ -0,0 +1,39 @@
+package com.example.rest_crud_app09.entity;
+
+public class Student {
+
+    private String firstName;
+
+    private String lastName;
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName  = lastName;
+    }
+
+    // getters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    // setters
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
