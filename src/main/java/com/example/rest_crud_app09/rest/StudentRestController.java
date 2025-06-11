2f84a3abd9b0740aa25739615edf357568409253
Sorin Gore <soringoredev@gmail.com>
2025-06-11 20:38:01 +0300

    chore: Add pj

--- src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
+++ src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.rest_crud_app09.rest;
 
 import com.example.rest_crud_app09.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,35 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
 
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
 
-        theStudents.add(new Student("Munteanu", "Eugen"));
+
+    // definim @PostConstructor ca sa incercam studentii nostri o singura data
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Muntean", "Eugen"));
         theStudents.add(new Student("Ojog", "Maria"));
         theStudents.add(new Student("Gonzales", "Pedro"));
 
+
+    }
+
+    // definim endpointu-ul pentru students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    // definim un endoint ca sa afisam toti studentii nostri dupa index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return theStudents.get(studentId);
+
     }
 }
